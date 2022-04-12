9ecc08ce1228d5c86d613b9b498522bb67241441
Shahrukh Khan <shahrukh.khan@mobilecoderz.com>
2022-04-12 13:29:17 +0530

    Entries managed

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get('/', async(req, res) => {
     var meta = new AWS.MetadataService();
     meta.request("/latest/meta-data/instance-id", function(err, data) {
         console.log(data);
-        let instanceId = data || 'NA';
-        //let instanceId = 'i-091fec15b42cc7e6b';
+        //let instanceId = data || 'NA';
+        let instanceId = 'i-076acd439f4d65d6a';
         // Make a request for a user with a given ID
         let url = `http://api-dev.kwikpic.in/api/app/ec2/entries/${instanceId}`;
         axios.get(url)
@@ -20,7 +20,18 @@ app.get('/', async(req, res) => {
                     apidata: response.data,
                     instanceId: instanceId
                 }
-                res.json(respdata);
+                let html = `<h1>Instance id-${instanceId}</h1><br>
+                <h2>Following are the assigned entries:-<br>`;
+
+                let lists = `<ol>`
+                console.log(response.data.data.instanceEntries, 'dfadf')
+                for (var key in response.data.data.instanceEntries) {
+                    console.log(key, response.data.data.instanceEntries[key], 'sss');
+                    lists += `<li><p style='color:red;'>Key---->${key}</p> value- ${response.data.data.instanceEntries[key]}</li>`;
+                }
+                lists += '</ol>';
+                html += lists;
+                res.send(html);
             })
             .catch(function(error) {
                 // handle error
