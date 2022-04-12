c4ef94c0f81de5c862484c907d2c1a8f99eee32e
Shahrukh Khan <shahrukh.khan@mobilecoderz.com>
2022-04-12 10:47:57 +0530

    url changed

--- app.js
+++ app.js
@@ -8,18 +8,22 @@ app.get('/', async(req, res) => {
     var meta = new AWS.MetadataService();
     meta.request("/latest/meta-data/instance-id", function(err, data) {
         console.log(data);
+        let instanceId = data;
+        //let instanceId = 'i-091fec15b42cc7e6b';
         // Make a request for a user with a given ID
-        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        let url = `http://api-dev.kwikpic.in/api/app/ec2/entries/${instanceId}`;
+        axios.get(url)
             .then(function(response) {
                 // handle success
                 console.log(response.data);
+                res.json(response.data);
             })
             .catch(function(error) {
                 // handle error
                 console.log(error);
             });
     });
-    res.send('<h1>Auto Scaling Demo</h1>');
+
 })
 
 app.listen(port, () => {
